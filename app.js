b980735de5c9fee668db80bb24d777dbf70592cf
Yoann-P-ekypia <yoann@ekypia.fr>
2022-07-08 15:31:14 +0200

    suite du projet

--- app.js
+++ app.js
@@ -1,24 +1,52 @@
+// import des modules npm - Ajout des plugins externes
 const express = require('express');
 
+// Création d'une application express
 const app = express();
 
+app.use(express.json());
+
+
+
+// On importe mongoose pour pouvoir utiliser la base de données
+const mongoose = require('mongoose');
+
+mongoose.connect('openclassroomsyoannprojet6://localhost:<Graphiste3825>@admin.mongodb.net',
+    { useNewUrlParser: true,
+        useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
 
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-    next();
-});
 
-app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
+app.get('/api/auth/signup', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'Mon premier objet',
+            description: 'Les infos de mon premier objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 4900,
+            userId: 'qsomihvqios',
+        },
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'Mon deuxième objet',
+            description: 'Les infos de mon deuxième objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 2900,
+            userId: 'qsomihvqios',
+        },
+    ];
+    res.status(200).json(stuff);
 });
 
 module.exports = app;
